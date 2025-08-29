444aa5f2439741085e9bbde89b4b4ae71c340755
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-08-29 14:26:27 +0700

    feat: konfigurasi extension

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -7,7 +7,6 @@ function Navbar() {
         <h1 className="text-xl font-bold">User Dashboard</h1>
         <p className="text-sm text-gray-300">Welcome, User</p>
       </div>
-
       <button className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-lg transition">
         Logout
       </button>
