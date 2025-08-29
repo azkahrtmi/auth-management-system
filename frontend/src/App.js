4873dbfae142a153cd0aee5d3fcba0d613400b7d
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-08-29 09:45:14 +0700

    feat: install tailwind dan penambahan folder component untuk frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import "./index.css";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
         >
           Learn React
         </a>
+        <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>
       </header>
     </div>
   );
