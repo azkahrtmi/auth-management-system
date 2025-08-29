2975e528f39e14bc49978a3818ba53a345ce6e61
RezaPahlevi1 <flashwatchreza@gmail.com>
2025-08-29 20:06:03 +0700

    fix: perbaikan warna navbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,3 @@
-import Navbar from "./components/Navbar";
 import DashboardLayout from "./layout/DashboardLayout";
 import DashboardAdmin from "./pages/DashboardAdmin";
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -2,9 +2,18 @@ import { MdLogout } from "react-icons/md";
 
 function Navbar({ role }) {
   return (
-    <nav className="flex justify-between items-center bg-green-50 text-black px-6 py-4 shadow-md">
+    <nav
+      style={{
+        background:
+          "linear-gradient(to bottom, rgba(173, 186, 218, 0.8), rgba(61, 82, 161, 0.6))",
+        color: "white",
+        padding: "16px",
+        boxShadow: "0 4px 6px rgba(0, 0, 0, 0.1)",
+      }}
+      className="flex justify-between items-center text-black px-6 py-4 shadow-md"
+    >
       <div>
-        <h1 className="text-xl font-bold">{role} Dashboard</h1>
+        <h1 className="text-xl font-bold text-black">{role} Dashboard</h1>
         <p className="text-sm text-black">Welcome, {role}</p>
       </div>
       <button className="bg-[#EEE7F6] hover:bg-[#ADBADA] cursor-pointer text-black font-semibold px-4 py-2 rounded-lg transition flex justify-center items-center gap-2">
