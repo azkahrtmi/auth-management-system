4b4f4415c6b2351bb0bfa9af59da727bfa9c3505
azkahrtmi <azkahartami10@gmail.com>
2025-09-03 23:47:07 +0700

    fix: change "" to ``

--- frontend/src/components/ContainerForm.jsx
+++ frontend/src/components/ContainerForm.jsx
@@ -11,7 +11,7 @@ function ContainerForm() {
     e.preventDefault();
 
     try {
-      const res = await fetch("${import.meta.env.VITE_API_URL}/auth/login", {
+      const res = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ email, password }),
