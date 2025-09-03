fccf87a869dfc2fb6895d9c1ebc8e15b53bb85e3
azkahrtmi <azkahartami10@gmail.com>
2025-09-03 23:44:20 +0700

    breaking change : lol

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
-import authRoutes from "./routes/auth";
-import adminRoutes from "./routes/admin";
+import authRoutes from "../src/routes/auth";
+import adminRoutes from "../src/routes/admin";
 import cors from "cors";
 
 dotenv.config();
@@ -16,11 +16,7 @@ app.use(
   })
 );
 
-// route
 app.use("/auth", authRoutes);
 app.use("/admin", adminRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+export default app;
